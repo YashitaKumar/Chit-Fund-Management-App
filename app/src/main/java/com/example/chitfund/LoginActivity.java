f907029b20c056d850079b07e29d6ee3b872f333
Manikanta <mandala.manikanta.20cse@bmu.edu.in>
2022-06-17 15:19:02 +0530

    17-06-2022 registrations functionality is done, and also connection with firebase

--- app/src/main/java/com/example/chitfund/LoginActivity.java
+++ app/src/main/java/com/example/chitfund/LoginActivity.java
@@ -5,9 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class LoginActivity extends AppCompatActivity {
+    private EditText emailID;
+    private EditText password;
+    private Button logIn;
+    private TextView forgotPaasword;
+    private TextView signUP;
+
+    //firebase
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,16 +27,34 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login_activity);
         TextView btnSignUp = findViewById(R.id.btn_SignUp);
         TextView btnForgotPassword = findViewById(R.id.btn_ForgotPassword);
-        btnForgotPassword.setOnClickListener(new View.OnClickListener() {
+        logIn();
+    }
+
+    private void logIn() {
+        emailID = findViewById(R.id.input_Email);
+        password = findViewById(R.id.editTextTextPassword);
+        logIn = findViewById(R.id.btnLogin);
+        forgotPaasword = findViewById(R.id.btn_ForgotPassword);
+        signUP = findViewById(R.id.btn_SignUp);
+        forgotPaasword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, VerifyEmailActivity.class));
             }
         });
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        signUP.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,MainActivity.class));
+            }
+        });
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String EmailId = emailID.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+
+//                startActivity(new intent(LoginActivity.this, homepage.class));
             }
         });
     }
--- app/src/main/java/com/example/chitfund/MainActivity.java
+++ app/src/main/java/com/example/chitfund/MainActivity.java
@@ -1,24 +1,120 @@
 package com.example.chitfund;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    private Button registrationButton;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText emailID;
+    private EditText PhoneNo;
+    private EditText Password;
+    private TextView signUp;
+    private EditText rePassword;
+
+    private ProgressDialog progDialog;
+
+    //firebase:
+    private FirebaseAuth Authen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView btn = findViewById(R.id.btn_SignIn);
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        Authen = FirebaseAuth.getInstance();
+        progDialog = new ProgressDialog(this);
+
+        registration();
+    }
+    private void registration(){
+        registrationButton = findViewById(R.id.btnRegister);
+        firstName = findViewById(R.id.input_firstName);
+        lastName = findViewById(R.id.input_lastName);
+        emailID = findViewById(R.id.input_Email);
+        PhoneNo = findViewById(R.id.input_phoneNo);
+        Password = findViewById(R.id.input_password);
+        rePassword = findViewById(R.id.input_password2);
+        signUp = findViewById(R.id.btn_SignIn);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
             }
         });
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fName= firstName.getText().toString().trim();
+                String lName= lastName.getText().toString().trim();
+                String EmailId = emailID.getText().toString().trim();
+                String phoneNo = PhoneNo.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+                String rePass = rePassword.getText().toString().trim();
+                if(TextUtils.isEmpty(fName)){
+                    firstName.setError("please fill first name");
+                    return;
+                }
+                if(TextUtils.isEmpty(lName)){
+                    lastName.setError("please fill last name");
+                    return;
+                }
+                if(TextUtils.isEmpty(EmailId)){
+                    emailID.setError("please fill email id");
+                    return;
+                }
+                if(TextUtils.isEmpty(phoneNo)){
+                    PhoneNo.setError("please fill phone number");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("please enter the password");
+                    return;
+                }
+                if(TextUtils.isEmpty(rePass)){
+                    rePassword.setError("please reenter the password");
+                    return;
+                }
+                if(!password.equals(rePass)){
+                    rePassword.setError("passwords are not matching!");
+                }
+
+                progDialog.setMessage("Processing..");
+
+                Authen.createUserWithEmailAndPassword(EmailId,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isComplete()){
+                            progDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Registration Complete!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this,HomeActivity.class));
+                        }
+                        else{
+                            progDialog.dismiss();
+                            Toast.makeText(MainActivity.this, "Registation Faild!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
+
 }

e5fc174a323c6ab2356b649aff96329c493f6fa3
Yashita Kumar <yashita149@gmail.com>
2022-06-29 22:06:17 +0530

    Chit Fund App User Management Module

---
+++ app/src/main/java/com/example/chitfund/DashboardActivity.java
@@ -0,0 +1,15 @@
+package com.example.chitfund;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}

72813f69a22c6a63e8e93b44410b60ba303c68b6
Manikanta <mandala.manikanta.20cse@bmu.edu.in>
2022-07-05 03:13:27 +0530

    change profile picture is added

--- app/src/main/java/com/example/chitfund/DashboardActivity.java
+++ app/src/main/java/com/example/chitfund/DashboardActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -18,19 +23,25 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.view.Change;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 public class DashboardActivity extends AppCompatActivity implements View.OnClickListener{
 
     private CardView LogoutButton;
+    private CardView ChangeProfileImage;
     private TextView PhoneNumber;
     private TextView EmailId;
     private TextView UserName;
     private TextView UserId;
 
-    private FirebaseUser user;
+    private ShapeableImageView shapeableImageViewProfileImage;
+
     private FirebaseAuth mAuth;
-    private FirebaseDatabase database;
-    private DatabaseReference userRef;
+    private StorageReference storageReference;
+    private StorageReference profileReference;
 
     private String userID;
 
@@ -40,6 +51,9 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        ChangeProfileImage = findViewById(R.id.CRUD);
+        shapeableImageViewProfileImage = findViewById(R.id.shapeableImageViewProfileImage);
+
         PhoneNumber = findViewById(R.id.TextViewUserPhoneNumber);
         EmailId = findViewById(R.id.TextViewUserEmailId);
         UserName = findViewById(R.id.TextViewUserName);
@@ -48,33 +62,21 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
         LogoutButton = findViewById(R.id.Logout);
 
         mAuth = FirebaseAuth.getInstance();
-//        user = FirebaseAuth.getInstance().getCurrentUser();
-//        userRef = FirebaseDatabase.getInstance().getReference();
-//        userID = user.getUid();
-//
-//        userRef.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot snapshot) {
-//                User userProfile = snapshot.getValue(User.class);
-//                if(userProfile != null){
-//                    String fName = userProfile.getfName();
-//                    String lName = userProfile.getlName();
-//                    String phoneNumber = userProfile.getPhoneNumber();
-//                    String emailId = userProfile.getEmailId();
-//
-//                    PhoneNumber.setText("Phone No.: "+phoneNumber);
-//                    UserName.setText(fName+lName);
-//                    EmailId.setText("Email ID: "+ emailId);
-//                    UserId.setText("UID: "+userID);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError error) {
-//                Toast.makeText(DashboardActivity.this, "Something's Wrong!", Toast.LENGTH_SHORT).show();
-//            }
-//        });
-//
+
+        storageReference = FirebaseStorage.getInstance().getReference();
+        profileReference = storageReference.child("Users/"+mAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(shapeableImageViewProfileImage);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(DashboardActivity.this, "Failed to load the profile picture!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
 
         String emailId= mAuth.getCurrentUser().getEmail().toString().trim();
         EmailId.setText("Email ID: "+ emailId);
@@ -110,6 +112,7 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
 
 
         LogoutButton.setOnClickListener(this);
+        ChangeProfileImage.setOnClickListener(this);
     }
 
     private void UserLogOut() {
@@ -124,7 +127,7 @@ public class DashboardActivity extends AppCompatActivity implements View.OnClick
                 UserLogOut();
                 break;
             case (R.id.CRUD):
-
+                startActivity(new Intent(this,ChangeProfilePicture.class));
                 break;
         }
     }
